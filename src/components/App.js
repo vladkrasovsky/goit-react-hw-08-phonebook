5d01a384ae260d943a8bc9fcea8339d84e4b98d9
Vlad Krasovsky <v.o.krasovsky@gmail.com>
2023-01-21 11:26:26 +0200

    Adds basic routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,25 @@
+import { lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { GlobalStyle } from './GlobalStyle';
 import { Layout } from './Layout';
+
+const HomePage = lazy(() => import('pages/Home'));
+const RegisterPage = lazy(() => import('pages/Register'));
+const LoginPage = lazy(() => import('pages/Login'));
+const ContactsPage = lazy(() => import('pages/Contacts'));
+const NotFoundPage = lazy(() => import('pages/NotFound.js'));
 
 export const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Layout />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/contacts" element={<ContactsPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
       </Routes>
       <GlobalStyle />
     </>
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,13 @@
+import { Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
+
 export const Layout = () => {
   return (
     <div>
       <p>AppBar</p>
+      <Suspense fallback={<div>Loading page...</div>}>
+        <Outlet />
+      </Suspense>
     </div>
   );
 };
